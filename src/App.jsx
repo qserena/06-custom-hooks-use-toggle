6ad6f9baa62af39701e4bffc1804eb62fc213a91
Tom Serenander <tom@serenander.se>
2023-11-29 09:21:57 +0100

    Re-entered Menu in App()

--- src/App.jsx
+++ src/App.jsx
@@ -9,44 +9,20 @@ function App() {
 	return (
 		<>
 			<Star />
+
+			<br />
+
+			<Menu>
+				<Menu.Button>Menu</Menu.Button>
+				<Menu.Dropdown>
+					<Menu.Item>Home</Menu.Item>
+					<Menu.Item>About</Menu.Item>
+					<Menu.Item>Contact</Menu.Item>
+					<Menu.Item>Blog</Menu.Item>
+				</Menu.Dropdown>
+			</Menu>
 		</>
 	)
 }
-
-// Using Toggle.Display for the render props
-// <Toggle onToggle={() => {console.log("Toggled")}}>
-//   <Toggle.Button>
-//     <Toggle.Display>
-//       {(on) => {
-//         return <div className={`box ${on ? "filled" : ""}`}></div>
-//       }}
-//     </Toggle.Display>
-//   </Toggle.Button>
-// </Toggle>
-// Example using Toggle.On and Toggle.Off to conditionally render items
-// <Toggle.On>
-//   <div className="box filled"></div>
-// </Toggle.On>
-// <Toggle.Off>
-//   <div className="box"></div>
-// </Toggle.Off>
-
-// Simple Star component that uses the Toggle component internally
-// <Star />
-
-// <br />
-
-// Menu Component using Toggle component internally to manage
-// state and context
-
-// <Menu>
-//   <Menu.Button>Menu</Menu.Button>
-//   <Menu.Dropdown>
-//     <Menu.Item>Home</Menu.Item>
-//     <Menu.Item>About</Menu.Item>
-//     <Menu.Item>Contact</Menu.Item>
-//     <Menu.Item>Blog</Menu.Item>
-//   </Menu.Dropdown>
-// </Menu>
 
 export default App
